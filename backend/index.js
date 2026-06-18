8a345dca3c6e6a0670eab0cd2aaf7bba59142346
bargali-dev <bargali.dev09@gmail.com>
2026-06-18 13:15:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -102,7 +102,7 @@ io.on("connection", (socket) => {
 });
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://bharatbnb-frontend.onrender.com", credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
