db4ff117c22e75d6229de6151d98b7025b696cbe
bargali-dev <bargali.dev09@gmail.com>
2026-06-18 13:29:15 +0530

    Update Socket.jsx

--- frontend/src/Context/Socket.jsx
+++ frontend/src/Context/Socket.jsx
@@ -1,6 +1,6 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8000", {
+export const socket = io("https://bharat-backend-44yj.onrender.com", {
   withCredentials: true,
 });
