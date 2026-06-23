191b65531c8a10647d88367e75bfc5637b20b135
bargali-dev <bargali.dev09@gmail.com>
2026-06-23 10:36:24 +0530

    Update buddies.Modules.js

--- backend/Modules/buddies.Modules.js
+++ backend/Modules/buddies.Modules.js
@@ -2,33 +2,28 @@ import mongoose from "mongoose";
 
 const buddySchema = new mongoose.Schema(
   {
-    // 👤 Current User (who is searching)
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
 
-    // 👤 Selected Buddy (after matching)
     selectedUser: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       default: null,
     },
 
-    // 🏠 Property reference (VERY IMPORTANT)
     property: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Listing",
       required: true,
     },
 
-    // 📍 Location
     city: {
       type: String,
     },
 
-    // 📅 Dates
     checkIn: {
       type: String,
       required: true,
@@ -39,20 +34,18 @@ const buddySchema = new mongoose.Schema(
       required: true,
     },
 
-    // 🚻 Optional preference
     gender: {
       type: String,
       enum: ["male", "female", "any"],
       default: "any",
     },
-    // 🔥 NEW TYPE (IMPORTANT)
     // type: {
     //   type: String,
     //   enum: ["search", "request"],
     //   default: "search",
     // },
 
-    // 🔥 NEW STATUS (IMPORTANT)
+
     status: {
       type: String,
       enum: ["searching", "matched", "booked","accepted","rejected"],
